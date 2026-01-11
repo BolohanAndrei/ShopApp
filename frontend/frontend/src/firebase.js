0654cc2595e684e58573c5539eb1a925b454ab80
BolohanAndrei <bolohanandrei769@gmail.com>
2026-01-11 23:24:42 +0200

    firebase edited

--- frontend/frontend/src/firebase.js
+++ frontend/frontend/src/firebase.js
@@ -3,19 +3,19 @@ import { initializeFirestore } from "firebase/firestore";
 
 // 👇 PUNE DATELE REALE AICI (doar pentru test)
 const firebaseConfig = {
-  apiKey: "AIzaSyB7GUNQER2jkCxWGzDgrxkwje-NjKf3KE4",
-  authDomain: "shopchat-48c0a.firebaseapp.com",
-  projectId: "shopchat-48c0a",
-  storageBucket: "shopchat-48c0a.firebasestorage.app",
-  messagingSenderId: "893741054544",
-  appId: "1:893741054544:web:2c6222ee995c6a5a9daf55",
-  measurementId: "G-CW37R6H1QJ"
+  apiKey: "AIzaSyCl-tbqbfT4URsZP54JDBLQw04aZjtFCbw",
+  authDomain: "shopapp-chat.firebaseapp.com",
+  projectId: "shopapp-chat",
+  storageBucket: "shopapp-chat.firebasestorage.app",
+  messagingSenderId: "481019734064",
+  appId: "1:481019734064:web:497de01469957f5be265f5",
+  measurementId: "G-45HDK8BP5P",
 };
 const app = initializeApp(firebaseConfig);
 
 // Păstrăm setarea pentru conexiune stabilă
 export const db = initializeFirestore(app, {
-    experimentalForceLongPolling: true,
+  experimentalForceLongPolling: true,
 });
 
 console.log("Firebase conectat la proiectul:", firebaseConfig.projectId);
